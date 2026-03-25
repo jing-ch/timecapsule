90a470e06d0a7bed456b72cbb137a53415e92bc0
jing <chenjinghanlaura@gmail.com>
2026-03-25 01:09:05 -0700

    added template files for auth. not yet implemented.

---
+++ TimeCapsule/app/src/main/java/edu/northeastern/timecapsule/auth/RegisterActivity.java
@@ -0,0 +1,27 @@
+package edu.northeastern.timecapsule.auth;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import edu.northeastern.timecapsule.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    // todo: Jinghan will implement this
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_register);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
